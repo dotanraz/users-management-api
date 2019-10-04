1c09e785ef4667e153db1e2289e0ed760df94405
squid12 <dotanrz@gmail.com>
2019-10-04 15:41:28 +0300

    add restartPostgresDb.sh file

---
+++ tools/restartPostgresDb.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+########################################
+# start or restart postgres db container
+# properties:
+# port: 5432
+# user: postgres
+# password: 1234
+########################################
+
+echo stop existing postgres db if exist...
+docker stop postgres1
+sleep 5
+echo run docker prune to remove old containers not in use
+docker system prune -f
+echo hard un-binding port 5432...
+sudo kill -9 `sudo lsof -t -i:5432`
+echo start postgres container...
+docker run --name postgres1 -e POSTGRES_PASSWORD=1234 -p 5432:5432 -d postgres
+docker ps
